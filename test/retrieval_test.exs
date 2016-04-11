31c688aaf1b31162330b9045697b4877be304e09
Robbie <deibbor@gmail.com>
2016-04-10 22:22:49 -0400

    Wrote unit tests for creating tries and testing for membership.

--- test/retrieval_test.exs
+++ test/retrieval_test.exs
@@ -2,7 +2,20 @@ defmodule RetrievalTest do
   use ExUnit.Case
   doctest Retrieval
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  @test_data ~w/apple apply ape bed between betray cat cold hot
+                warm winter maze smash crush under above people
+                negative poison place out divide zebra extended/
+
+  @test_trie Retrieval.new(@test_data)
+
+  test "empty trie" do
+    assert Retrieval.new == %Retrieval.Trie{}
   end
+
+  test "contains?" do
+    assert Retrieval.contains?(@test_trie, "apple") == true
+    assert Retrieval.contains?(@test_trie, "smash") == true
+    assert Retrieval.contains?(@test_trie, "abcde") == false
+  end
+
 end
