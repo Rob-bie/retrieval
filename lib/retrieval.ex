58008dec16b0cd26fd6c3b13c003691affebb316
Robbie <deibbor@gmail.com>
2016-04-10 21:59:26 -0400

    Implemented functionality for creating tries and inserting binaries.

--- lib/retrieval.ex
+++ lib/retrieval.ex
@@ -1,2 +1,34 @@
 defmodule Retrieval do
+
+  alias Retrieval.Trie
+
+  def new, do: %Trie{}
+
+  def new(binaries) when is_list(binaries) do
+    insert(%Trie{}, binaries)
+  end
+
+  def new(binary) when is_binary(binary) do
+    insert(%Trie{}, binary)
+  end
+
+  def insert(%Trie{trie: trie}, binaries) when is_list(binaries) do
+    %Trie{trie: Enum.reduce(binaries, trie, &_insert(&2, &1))}
+  end
+
+  def insert(%Trie{trie: trie}, binary) when is_binary(binary) do
+    %Trie{trie: _insert(trie, binary)}
+  end
+
+  defp _insert(trie, <<next, rest :: binary>>) do
+    case Map.has_key?(trie, next) do
+      true  -> Map.put(trie, next, _insert(trie[next], rest))
+      false -> Map.put(trie, next, _insert(%{}, rest))
+    end
+  end
+
+  defp _insert(trie, <<>>) do
+    Map.put(trie, :mark, :mark)
+  end
+
 end
