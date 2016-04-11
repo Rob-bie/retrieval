700480fe5cce45a362b2a804ef5c934733461ee4
Robbie <deibbor@gmail.com>
2016-04-11 10:26:49 -0400

    Added ExDoc dependency for documentation.

--- mix.exs
+++ mix.exs
@@ -10,23 +10,12 @@ defmodule Retrieval.Mixfile do
      deps: deps]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:earmark, "~> 0.1", only: :dev},
+     {:ex_doc, "~> 0.11", only: :dev}]
   end
 end
